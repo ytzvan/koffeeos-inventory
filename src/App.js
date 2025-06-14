9bf8eb982589f983386cef18cd966503ccff6c05
Ytzvan Mastino <mastino14@gmail.com>
2025-06-14 13:16:04 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo desde Pulpa Coffee Co.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pulpa.coffee"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Us
         </a>
       </header>
     </div>
