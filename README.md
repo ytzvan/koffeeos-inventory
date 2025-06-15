5c99dbace2610138f4f58426d02758de4a90ab33
Ytzvan Mastino <mastino14@gmail.com>
2025-06-15 12:36:56 -0500

    Update meta, README, and tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# KoffeeOS Inventory UI
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and customized for the KoffeeOS inventory system.
 
 ## Available Scripts
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Visit Us link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/visit us/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders greeting message', () => {
+  render(<App />);
+  const greetingElement = screen.getByText(/Hola Mundo desde Pulpa Coffee Co./i);
+  expect(greetingElement).toBeInTheDocument();
 });
